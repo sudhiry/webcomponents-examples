162b695e70a008fa0ed1e86fdf9e520d212332d3
Sudhir <sudhiryelikar@gmail.com>
2019-07-30 19:06:26 +0530

    Updates and security fixes

--- examples/react-web-component/src/SampleComponent.js
+++ examples/react-web-component/src/SampleComponent.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default class SampleComponent extends React.Component {
     render() {
-        return (<div>This is react sample Component! I can do anything in it</div>);
+        const styles = {
+            border: '1px solid'
+        }
+        return (<div style={styles} >This is react sample Component! I can do anything in it</div>);
     }
 }
